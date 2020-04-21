b99cd547d711b7f17ea1baffc3500fd8ca3d7b98
seonghunmeow <zser229@gmail.com>
2020-04-21 21:21:08 +0900

    add Filter / UI

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import TableCell from '@material-ui/core/TableCell'; 
 import TableRow from '@material-ui/core/TableRow';
+import CustomerDelete from './CustomerDelete';
 
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <TableRow>
-                    <TableCell>{this.props.id}</TableCell>
-                    <TableCell><img src={this.props.image} alt="profile"/></TableCell>
-                    <TableCell>{this.props.name}</TableCell>
-                    <TableCell>{this.props.birthday}</TableCell>
-                    <TableCell>{this.props.gender}</TableCell>
-                    <TableCell>{this.props.job}</TableCell>
-                </TableRow>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+                <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
+            </TableRow>
         );
     }
 }
